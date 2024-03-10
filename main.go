dadfa13128eda2a81d602c9f175ff5868bd96393
annaturk <161259585+annaturk@users.noreply.github.com>
2024-03-09 20:51:51 -0600

    Update main.go

--- main.go
+++ main.go
@@ -8,37 +8,108 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	// OPTION 1 - Postgress application running on localhost
-	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=localhost sslmode=disable"
+var db *sql.DB
+
+// The main package can has the init function. 
+// The init function will be triggered before the main function
+
+func init() {
+	var err error
+
+	fmt.Println("Initializing the DB connection")
 
 	// Establish connection to Postgres Database
-	// db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=host.docker.internal sslmode=disable port=5433"
 
-	db, err := sql.Open("postgres", db_connection)
+	// OPTION 1 - Postgress application running on localhost
+	//db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=localhost sslmode=disable port = 5432"
+
+	// OPTION 2
+	// Docker container for the Postgres microservice - uncomment when deploy with host.docker.internal
+	//db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=host.docker.internal sslmode=disable port = 5433"
+
+	// OPTION 3
+	// Docker container for the Postgress microservice - uncomment when deploy with IP address of the container
+	// To find your Postgres container IP, use the command with your network name listed in the docker compose file as follows:
+	// docker network inspect cbi_backend
+	//db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=162.123.0.9 sslmode=disable port = 5433"
+
+	//Option 4
+	//Database application running on Google Cloud Platform.
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/ADD_YOUR_CONNECTION_NAME_FROM_GCP sslmode=disable port = 5432"
+
+	db, err = sql.Open("postgres", db_connection)
 	if err != nil {
-		fmt.Println("Error connecting to database:", err)
-		return
+		log.Fatal(fmt.Println("Couldn't Open Connection to database"))
+		panic(err)
 	}
 
 	// Test the database connection
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Couldn't connect to database:", err)
-		return
-	}
+	//err = db.Ping()
+	//if err != nil {
+	//	fmt.Println("Couldn't Connect to database")
+	//	panic(err)
+	//}
 
-	// Continuous data collection loop
-	for {
-		GetTaxiTrips(db)
-		GetShareTrips(db)
-		GetCOVIDCases(db)
-		GetCCVIData(db)
-		GetBuildingPermits(db)
-		GetUnemploymentRates(db)
-		GetNeighborhoodNames(db)
-
-		time.Sleep(24 * time.Hour) // Sleep for 24 hours before the next iteration
-	}
 }
 
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+func main() {
+
+
+	for {
+
+		// While using Cloud Run for instrumenting/prototyping/debugging use the server
+		// to trace the state of you running data collection services
+		// Navigate to Cloud Run services and find the URL of your service
+		// An example of your services URL: https://go-microservice-23zzuv4hksp-uc.a.run.app
+		// Use the browser and navigate to your service URL to to kick-start your service
+
+		log.Print("starting CBI Microservices ...")
+
+		go GetTaxiTrips(db)
+		go GetShareTrips(db)
+		go GetCOVIDCases(db)
+		go GetCCVIData(db)
+		go GetBuildingPermits(db)
+		go GetUnemploymentRates(db)
+		go GetNeighborhoodNames(db)
+
+		http.HandleFunc("/", handler)
+
+		// Determine port for HTTP service.
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8080"
+			log.Printf("defaulting to port %s", port)
+		}
+
+		// Start HTTP server.
+		log.Printf("listening on port %s", port)
+		log.Print("Navigate to Cloud Run services and find the URL of your service")
+		log.Print("Use the browser and navigate to your service URL to to check your service has started")
+
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(24 * time.Hour)
+	}
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("PROJECT_ID")
+	if name == "" {
+		name = "CBI-Project"
+	}
+
+	fmt.Fprintf(w, "CBI data collection microservices' goroutines have started for %s!\n", name)
+}
